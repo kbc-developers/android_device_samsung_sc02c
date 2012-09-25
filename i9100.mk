6298708780ea60137687e79e3cb63aed0bc9bdb6
sakuramilk <c.sakuramilk@gmail.com>
2012-09-25 17:18:09 +0900

    customize for SC-02C

--- i9100.mk
+++ i9100.mk
@@ -35,8 +35,12 @@ PRODUCT_PACKAGES += \
     GalaxyS2Settings
 
 # Sensors
-PRODUCT_PACKAGES += \
-    sensors.exynos4
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/usr/lib/sensors.exynos4.so:system/lib/hw/sensors.exynos4.so
+
+# Emoji
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/usr/lib/libemoji_docomo.so:system/lib/libemoji_docomo.so
 
 # Keylayout
 PRODUCT_COPY_FILES += \
